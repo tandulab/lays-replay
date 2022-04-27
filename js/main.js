91beb4e44bbe27f1c0ccf780214dad2c494b6b53
Sara <sarabertaina@hotmail.it>
2022-04-27 14:13:52 +0200

    add htaccess

--- js/main.js
+++ js/main.js
@@ -1,56 +1,60 @@
 $(function () {
+  if (document.querySelector("#main-header")) {
+    document.querySelector("#main-header").classList.add("pack-transform");
+  }
 
-  
-  document.querySelector('#main-header').classList.add('pack-transform');
+  if (document.getElementById("countDown")) {
+    // Set the date we're counting down to
+    var countDownDate = new Date("May 19, 2022 16:00:00").getTime();
 
-  // Set the date we're counting down to
-  var countDownDate = new Date("May 19, 2022 12:00:00").getTime();
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+      // Get today's date and time
+      var now = new Date().getTime();
 
-  // Update the count down every 1 second
-  var x = setInterval(function () {
-    // Get today's date and time
-    var now = new Date().getTime();
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      var html =
+        "<div class='days'><div>" +
+        days +
+        "</div><p>giorni</p></div>" +
+        "<div class='hours'><div>" +
+        hours +
+        "</div><p>ore</p></div>" +
+        "<div class='minutes'><div>" +
+        minutes +
+        "</div><p>minuti</p></div>" +
+        "<div class='seconds'><div>" +
+        seconds +
+        "</div><p>secondi</p></div>";
+
+      document.getElementById("countDown").innerHTML = html;
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countDown").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+  }
+
+  $("#link-il-progetto").on("click", function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#progetto").offset().top,
+      },
+      1000
     );
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    var html =
-      "<div class='days'><div>" +
-      days +
-      "</div><p>giorni</p></div>" +
-      "<div class='hours'><div>" +
-      hours +
-      "</div><p>ore</p></div>" +
-      "<div class='minutes'><div>" +
-      minutes +
-      "</div><p>minuti</p></div>" +
-      "<div class='seconds'><div>" +
-      seconds +
-      "</div><p>secondi</p></div>";
-
-    document.getElementById("countDown").innerHTML = html;
-
-    // If the count down is finished, write some text
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("countDown").innerHTML = "EXPIRED";
-    }
-  }, 1000);
-
-  $('#link-il-progetto').on('click', function () {
-    $('html,body').animate({
-               scrollTop: $("#progetto").offset().top
-           }, 1000);
-});
-
+  });
 });
 
 // When the user scrolls the page, execute myFunction
